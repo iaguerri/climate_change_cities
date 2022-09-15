36a9e30e90a8676960547cba3fe1871c5716968c
iaguerri <ireneaguerri@gmail.com>
2022-09-15 16:25:42 +0200

    vue vite working linux

--- climate-front/vite.config.js
+++ climate-front/vite.config.js
@@ -5,14 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    port: 3000
+  },
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server:{
-    host: true,
-    port: 5003
   }
 })
